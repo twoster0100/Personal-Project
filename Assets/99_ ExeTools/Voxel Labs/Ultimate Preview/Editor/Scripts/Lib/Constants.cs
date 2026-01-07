ceb639fbded26a85d478b1b0020d560bcb845ec3
twoster0100 <sk01020402096@gmail.com>
2026-01-07 21:29:41 +0900

    ㅇㅇㅇㅇ

--- Assets/99_ ExeTools/Voxel Labs/Ultimate Preview/Editor/Scripts/Lib/Constants.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace VoxelLabs.UltimatePreview
-{
-    public class Constants : MonoBehaviour
-    {
-        public static readonly string ULTIMATE_PREVIEW_VERSION_NUMBER = "v1.3";
-    }
-}
